7eca16fc8ffa1c6bd915bf81c9730ba0d1418faa
Alvaro Condori <condorialvaro95@gmail.com>
2024-08-01 12:02:22 -0300

    add delete product

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -53,3 +53,22 @@ exports.updateProduct = async (req, res) => {
     res.status(500).json({success: false, message: "Server Error"});
   }
 };
+
+exports.deleteProduct = async (req, res) => {
+  // #swagger.tags=['Product']
+  const {id} = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({success: false, message: "Invalid Product ID"});
+  }
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({success: true, message: "Product deleted"});
+  } catch (error) {
+    console.log("error in deleting product:", error.message);
+    res.status(500).json({success: false, message: "Server Error"});
+  }
+};
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -3,11 +3,13 @@ const {
   getProduct,
   createProduct,
   updateProduct,
+  deleteProduct,
 } = require("../controllers/product.controllers");
 const router = express.Router();
 
 router.get("/", getProduct);
 router.post("/", createProduct);
 router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 module.exports = router;
