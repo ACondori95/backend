c1706ae8ffd4ce59c6550466a29fce7e9c6f5211
Alvaro Condori <condorialvaro95@gmail.com>
2024-08-01 11:17:49 -0300

    add create product

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const {getProduct} = require("../controllers/product.controllers");
+const {
+  getProduct,
+  createProduct,
+} = require("../controllers/product.controllers");
 const router = express.Router();
 
 router.get("/", getProduct);
+router.post("/", createProduct);
 
 module.exports = router;
