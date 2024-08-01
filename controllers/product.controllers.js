7e5efcbe747b79a0fa0017dc8d37e1ee367c9ff8
Alvaro Condori <condorialvaro95@gmail.com>
2024-08-01 11:31:16 -0300

    add update product

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Product = require("../models/product.model");
 
 exports.getProduct = async (req, res) => {
+  // #swagger.tags=['Product']
   try {
     const products = await Product.find({});
     res.status(200).json({success: true, data: products});
@@ -11,6 +12,7 @@ exports.getProduct = async (req, res) => {
 };
 
 exports.createProduct = async (req, res) => {
+  // #swagger.tags=['Product']
   const product = req.body;
 
   if (!product.name || !product.price || !product.image) {
@@ -26,6 +28,28 @@ exports.createProduct = async (req, res) => {
     res.status(201).json({success: true, data: newProduct});
   } catch (error) {
     console.error("Error in create product:", error.message);
+    res.status(500).json({success: false, message: "Server Error"});
+  }
+};
+
+exports.updateProduct = async (req, res) => {
+  // #swagger.tags=['Product']
+  const {id} = req.params;
+
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({success: false, message: "Invalid Product ID"});
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({success: true, data: updatedProduct});
+  } catch (error) {
     res.status(500).json({success: false, message: "Server Error"});
   }
 };
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const {
   getProduct,
   createProduct,
+  updateProduct,
 } = require("../controllers/product.controllers");
 const router = express.Router();
 
 router.get("/", getProduct);
 router.post("/", createProduct);
+router.put("/:id", updateProduct);
 
 module.exports = router;
